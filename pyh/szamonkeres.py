d8464b8a8db9a8c4d377e4b4fe90edee7fb530c2
Majoros Máté Martin <Majorosmm@KKSZKI.LOCAL>
2022-12-01 13:15:15 +0100

    4.feladat

---
+++ pyh/szamonkeres.py
@@ -0,0 +1,40 @@
+#1.feladat
+'''vez = input("add meg a vezetek neved!")
+ker = input("add meg a kereszt neved!")
+
+print(f"Üdvözöllek {vez} {ker}")
+print(f"Üdvözöllek {ker} {vez} ")
+'''
+#2.feladat
+'''
+szam = int(input('adj meg egy szamot!'))
+print(szam -1)
+print(szam +1)
+'''
+#3.feladat
+'''
+szam1 = int(input('adj meg egy szamot!'))
+szam2 = int(input('adj meg meg egy szamot!'))
+
+print(szam1+szam2)
+print(szam1-szam2)
+print(szam1*szam2)
+print(szam1/szam2)
+'''
+#4.feladat
+'''
+a = int(input("adj meg egy szamot!"))
+b = int(input("adj egy masik szamot!"))
+c = 2*a+3*b
+print(c)
+d = c-2*a-3*b
+print(d)
+'''
+#5.feladat
+
+import random
+x = random.randrange(1,21)
+paros []
+paratlan []
+
+if 
